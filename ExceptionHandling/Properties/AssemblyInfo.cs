16744c65553fc408091ee1eb038552f5cf49faf1
LakshmipriyaSreedharan <lakshmipriya21comp@student.mes.ac.in>
2025-01-08 11:01:02 +0530

    Lambda and linq example

---
+++ ExceptionHandling/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExceptionHandling")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExceptionHandling")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b398682-f2e0-4cca-b57d-cd3a2f56c5c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LambdaAndLinq/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAndLinq
+{
+    internal class Program
+    {
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Department { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>
+            {
+                new Employee{Id=1,Name="Lakshmi",Age=25,Department="Computer"},
+                new Employee{Id=2,Name="Raj",Age=40,Department="Ecs"},
+                new Employee{Id=3,Name="Sakshi",Age=48,Department="Automobile"},
+                new Employee{Id=4,Name="Ambruta",Age=26,Department="IT"},
+                new Employee{Id=6,Name="Ayesha",Age=35,Department="Computer"},
+                new Employee{Id=7,Name="Viji",Age=50,Department="Ecs"},
+                new Employee{Id=8,Name="Soham",Age=34,Department="Computer"},
+                new Employee{Id=9,Name="Pranita",Age=23,Department="IT"}
+            };
+
+            var result = list.Where(e => e.Age>30).OrderBy(o => o.Name).Select(v => new {v.Name,v.Age});
+            foreach (var e in result)
+            {
+                Console.WriteLine($"Employee name is:{e.Name} and age is {e.Age}");
+            }
+        }
+    }
+}
