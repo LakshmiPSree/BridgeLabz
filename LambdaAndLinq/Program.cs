8cce2fa30fa6bce0ffe054bd902c46ae5391adaf
LakshmipriyaSreedharan <lakshmipriya21comp@student.mes.ac.in>
2025-01-09 11:23:10 +0530

    Added Person data management and updated lambda and linq

--- LambdaAndLinq/Program.cs
+++ LambdaAndLinq/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LambdaAndLinq
 {
@@ -15,6 +19,16 @@ namespace LambdaAndLinq
             public int Age { get; set; }
             public string Department { get; set; }
         }
+        public class Product
+        {
+            public Double price { get; set; }
+            public String name { get; set; }
+            public Product(Double price, String name)
+            {
+                this.price = price;
+                this.name = name;
+            }
+        }
         static void Main(string[] args)
         {
             List<Employee> list = new List<Employee>
@@ -26,14 +40,74 @@ namespace LambdaAndLinq
                 new Employee{Id=6,Name="Ayesha",Age=35,Department="Computer"},
                 new Employee{Id=7,Name="Viji",Age=50,Department="Ecs"},
                 new Employee{Id=8,Name="Soham",Age=34,Department="Computer"},
-                new Employee{Id=9,Name="Pranita",Age=23,Department="IT"}
+                new Employee{Id=9,Name="Pranita",Age=23,Department="IT"},
+                new Employee{Id=10,Name="Pranshu", Age=39,Department="IT"}, 
+                new Employee{Id=11,Name="Kiran",Age=21,Department="Literature"}
             };
 
-            var result = list.Where(e => e.Age>30).OrderBy(o => o.Name).Select(v => new {v.Name,v.Age});
+            var result = list.Where(e => e.Age>30).OrderBy(o => o.Name);
             foreach (var e in result)
             {
                 Console.WriteLine($"Employee name is:{e.Name} and age is {e.Age}");
             }
+
+
+            //Solving other problems 
+            //1.Write a program that counts the occurrences of a specific character in a list of strings using a lambda expression.
+            Console.WriteLine("Enter any statement");
+            string statement= Console.ReadLine();
+            Console.WriteLine("Enter the character which you want to count in the above given statement");
+            char character=char.Parse(Console.ReadLine());
+            int count = statement.Count(e => e == character);
+            Console.WriteLine($"The character {character} appears {count} times.");
+
+            //2.Write a program that performs various aggregate operations (sum, average, minimum, maximum)
+            //on a list of numbers using lambda expressions.
+            int[] arr = new int[] { 6, 9, 12, 45, 2, 8, 6, 3, };
+
+            Console.WriteLine("Calculating the sum of the numbers in the array { 6, 9, 12, 45, 2, 8, 6, 3, }");
+            int sum=arr.Sum(x=>x);
+            Console.WriteLine(sum);
+            Console.WriteLine("Calculating the average of the same numbers");
+            double avg = arr.Average(x=>x);
+            Console.WriteLine(avg);
+            Console.WriteLine("Calculating the Minimum of the numbers");
+            int min=arr.Min(x=>x);
+            Console.WriteLine(min);
+            Console.WriteLine("Calculating the maximum of the numbers");
+            int max= arr.Max(x=>x);
+            Console.WriteLine(max);
+
+            //3.Write a program that finds all words in a list that start with the letter 'A' using LINQ.
+            Console.WriteLine("Names that start with A");
+            var startwith = list.Where(word => word.Name.StartsWith("A")).Select(word => word.Name).ToList();
+            foreach (var e in startwith) Console.WriteLine(e);
+            //4.Write a program that finds the top 3 highest scores from a list of integers using LINQ.
+            Console.WriteLine("Top 3 scores");
+            var scores = list.OrderByDescending(word => word.Age).Select(word=>word.Age).Take(3);
+            foreach (var e in scores) Console.WriteLine(e);
+            //5.Write a program that groups a list of Person objects by their age using LINQ.
+            var person = list.Where(e=>e.Age == 50).Select(word => word.Name);
+            foreach (var e in person) Console.WriteLine(e);
+            //6.Write a program that filters a list of Product objects to find those with a price greater than $100
+            //and then sorts them by name using LINQ.Each Product object has Name and Price properties.
+            List<Product> products = new List<Product>();
+            products.Add(new Product(35.2, "Cone IceCream"));
+            products.Add(new Product(1999, "Wide Jeans"));
+            products.Add(new Product(112.5, "Vanilla Family Pack"));
+            products.Add(new Product(95, "Coke"));
+            products.Add(new Product(59, "Chicps"));
+            products.Add(new Product(200, "Cheese"));
+            products.Add(new Product(20, "Jam"));
+            products.Add(new Product(599, "Top"));
+            products.Add(new Product(10, "Chocolate"));
+
+            var product = products.Where(e => e.price > 100).OrderBy(f => f.name);
+            foreach (var e in product) Console.WriteLine(e.name);
+
+
+
+
         }
     }
 }
---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement
+{   
+    internal class Program
+    {
+        public class Person
+        {
+            public int SSN { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public int Age { get; set; }
+
+            public Person(int ssn, string name, string address, int age)
+            {
+                this.SSN = ssn;
+                this.Name = name;
+                this.Address = address;
+                this.Age = age;
+            }
+        }
+        static void Main(string[] args)
+        {   
+            List<Person> list = new List<Person>();
+            list.Add(new Person(1, "Rahul", "Mumbai", 45));
+            list.Add(new Person(2, "Darshan", "Nerul", 78));
+            list.Add(new Person(3, "Anita", "Vashi", 96));
+            list.Add(new Person(4, "Prathamesh", "Panvel", 89));
+            list.Add(new Person(5, "viji", "Mumbai", 13));
+            list.Add(new Person(6, "vardhan", "Nerul", 38));
+            list.Add(new Person(7, "Anagha", "Vashi", 15));
+            list.Add(new Person(8, "Pratik", "Panvel", 19));
+            list.Add(new Person(9, "Ramesh", " Navi Mumbai", 45));
+            list.Add(new Person(10, "Deepak", "Nasik", 37));
+
+            var top2 = list.Where(e => e.Age > 60).Take(2);
+            Console.WriteLine("Retrieving top 2 records from the list whose age is less than 60");
+            foreach (var person in top2) { 
+                Console.WriteLine(person.Name);
+            }
+
+            var age13to18 = list.Where(e => e.Age >= 13 && e.Age <= 18);
+            Console.WriteLine("Retrieving all records from the list whose age is between 13 and 18");
+            foreach (var person in age13to18)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            var avg = list.Average(e => e.Age);
+            Console.WriteLine($"Average of the ages present in the list is : {avg}");
+        }
+    }
+}
